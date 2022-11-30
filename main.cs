93c694788c4f7d6d2f5f9810b4bafa1246671b74
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2022-11-30 01:45:19 +0000

    new code

--- main.cs
+++ main.cs
@@ -9,30 +9,45 @@ class Program
 {
     public static void Main(string[] args)
     {
-        // This function tells the user if they're eligible for discount
-        string day;
-        int age;
+        // This function tells the user the type of their triangle
+        int sideA;
+        int sideB;
+        int sideC;
 
-        Console.WriteLine("Insert your the day today and your age to see if you're eligible for discount.");
+        Console.WriteLine("Insert side lengths of a trianlge here and find out the type:");
         Console.WriteLine();
 
-        Console.WriteLine("Your age: ");
-        age = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("side A: ");
+        sideA = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine();
 
-        Console.WriteLine("today is: ");
-        day = Console.ReadLine();
+        Console.WriteLine("side B: ");
+        sideB = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine();
 
-        if ((day == "Tuesday" || day == "Thursday") || (age > 12 && age < 25))
-        {
-            Console.WriteLine("You're eligible for a discount");
+        Console.WriteLine("side C: ");
+        sideC = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine();
+
+      double angleA = Math.Acos((Math.Pow(sideB, 2) + Math.Pow(sideC, 2) - Math.Pow(sideA, 2)) / (2 * sideB * sideC)) *
+    (180 / Math.PI);
+        double angleB = Math.Acos((Math.Pow(sideC, 2) + Math.Pow(sideA, 2) - Math.Pow(sideB, 2)) / (2 * sideC * sideA)) * (180 / Math.PI);
+        double angleC =
+    Math.Acos((Math.Pow(sideA, 2) + Math.Pow(sideB, 2) - Math.Pow(sideC, 2)) / (2 * sideA * sideB)) * (180 / Math.PI);
+
+        double sumOfAngles = Math.Round(angleA, 2) + Math.Round(angleB, 2) + Math.Round(angleC, 2);
+
+        if (sumOfAngles == 180) {
+          if (sideA == sideB && sideB == sideC && sideC == sideA) {
+              Console.WriteLine("Your triangle is Equilateral.");
+          } else if (sideA == sideB || sideB == sideC || sideC == sideA) {
+              Console.WriteLine("Your triangle is Isosceles.");
+          } else {
+              Console.WriteLine("Your triangle is Scalene.");
+          }
+        } else {
+            Console.WriteLine("That's no triangle.");
         }
-        else
-        {
-            Console.WriteLine("You aren't eligible.");
-        }
-
         Console.WriteLine("\nDone.");
     }
 }
